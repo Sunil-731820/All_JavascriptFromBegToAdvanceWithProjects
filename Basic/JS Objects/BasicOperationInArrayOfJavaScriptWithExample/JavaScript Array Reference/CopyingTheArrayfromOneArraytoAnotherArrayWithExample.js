f3602cf29dc10639c3f81f5f9b9af3f70cff74d6
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2024-02-04 22:54:07 +0530

    Started With Js Array objects Statement In Javascript With EXample

---
+++ Basic/JS Objects/BasicOperationInArrayOfJavaScriptWithExample/JavaScript Array Reference/CopyingTheArrayfromOneArraytoAnotherArrayWithExample.js
@@ -0,0 +1,6 @@
+// Input array
+let array = [1, 2, 3, 4, 5, 6, 7];
+
+// placing at index position 0 the element
+// between index 3 and 6
+console.log("Array " + array.copyWithin(0, 3, 6));
---
+++ Basic/JS Objects/BasicOperationInArrayOfJavaScriptWithExample/JavaScript Array Reference/UseOfArray.entriesMethodWithExample.js
@@ -0,0 +1,21 @@
+let languages = ["HTML", "CSS", "JavaScript", "ReactJS"];
+let g = languages.entries();
+for (var x of g) {
+	console.log("The ValueOf The X is :=",x);
+}
+
+
+let arr = ["HTML", "CSS", "JS",
+		"Bootstrap", "PHP"];
+let seqn = arr.entries();
+console.log("Applying the Array entries method:");
+
+for (let x of seqn) {
+	console.log("The Value Of The X Is : for Seq : is :=",x);
+};
+
+
+const x1 = ["Geeks", "for", "Geeks"];
+
+for (const [index, element] of x1.entries())
+	console.log(index, element);
