72702289e672bfa07e678e45300cd83ed06dcbcd
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2024-02-04 21:47:29 +0530

    Started With Js Expression Statement In Javascript With EXample

---
+++ Basic/JS Objects/JavaScriptArraysWithFirstExample.js
@@ -0,0 +1,14 @@
+/*
+What is Array in JavaScript?
+JavaScript Array is a data structure that allows you to store and organize multiple values within a single variable. It is a versatile and dynamic object. It can hold various data types, including numbers, strings, objects, and even other arrays. Arrays in JavaScript are zero-indexed i.e. the first element is accessed with an index 0, the second element with an index of 1, and so forth.
+
+*/
+
+// Creating an Empty Array
+let names = [];
+console.log(names);
+
+// Creating an Array and Initializing with Values
+let courses = ["HTML", "CSS", "Javascript", "React"];
+
+console.log(courses);
---
+++ Basic/JS Objects/JavaScriptArraysWithSecondExample.js
@@ -0,0 +1,25 @@
+// Declaration of an empty array 
+// using Array constructor
+let names = new Array();
+console.log(names);
+
+// Creating and Initializing an array with values
+let courses = new Array("HTML", "CSS", "Javascript", "React");
+console.log(courses);
+
+// Initializing Array while declaring
+let arr = new Array(3);
+arr[0] = 10;
+arr[1] = 20;
+arr[2] = 30;
+console.log(arr);
+
+let arr1 = new Array(5);
+arr1[0] ="Sunil";
+arr1[1] ="Harish";
+arr1[2] ="Arti";
+arr1[3] ="Suman";
+arr1[4] ="Khsubhboo";
+console.log("The Array is : =")
+console.log(arr1);
+
