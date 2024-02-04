897a66b53159fe89a21c3d91210f37439aa5174b
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2024-02-04 16:07:01 +0530

    Started With Js Expression Statement In Javascript With EXample

--- Basic/JS Expression and Operators/FirstExampleOfAssignmentOperator.js
+++ Basic/JS Expression and Operators/FirstExampleOfAssignmentOperator.js
@@ -1,3 +1,23 @@
+
+/*
+JS Expression and Operators
+JavaScript expressions are the combinations of values, variables, and operators that can be evaluated to produce a result. Operators are symbols that perform operations on operands.
+
+Assignment operators
+Comparison operators
+Arithmetic operators
+Bitwise operators
+Logical operators
+BigInt Operators
+String operators
+Ternary operator
+Comma operator
+Unary operators
+Relational operators
+
+
+*/
+
 let a = 2;
 const b = 3;
 
