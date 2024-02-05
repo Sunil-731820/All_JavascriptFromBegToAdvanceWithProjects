51cbb163f9ca3f717e0fe7aa5488b75a480b7b17
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2024-02-05 21:55:49 +0530

    Started With Js Function With Example Statement In Javascript With EXample

---
+++ Basic/FunctionInJavaScript/FirstExampleOfFunction.js
@@ -0,0 +1,4 @@
+function calcAddition(number1, number2) { 
+	return number1 + number2; 
+}
+console.log(calcAddition(6,19));
---
+++ Basic/FunctionInJavaScript/SecondEXampleOfFunctionInJs.js
@@ -0,0 +1,5 @@
+const square = function (number) {
+	return number * number;
+};
+let x = square(4); // x gets the value 16
+console.log(x);
